7c2760d9ac5f99a38925eb0081efe0ae422bced5
deox03 <cavidqurbanov1503@gmail.com>
2025-05-13 11:51:04 +0400

    Son dəyişikliklər

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, NavLink, BrowserRouter } from "react-router-dom";
+import {Routes, Route, NavLink } from "react-router-dom";
 import Clock from "./Clock";
 import Timer from "./Timer";
 import Stopwatch from "./Stopwatch";
@@ -7,19 +7,20 @@ import styles from "./Nav.module.css";
 function App() {
   return (
 
-    <BrowserRouter>  
+ 
       <div className={styles.nav}>
         <NavLink to="/clock" >Saat</NavLink>
         <NavLink to="/timer" >Taymer</NavLink>
         <NavLink to="/stopwatch" >Saniyəölçən</NavLink>
-      </div>
-
-      <Routes>
+              <Routes>
         <Route path="/clock" element={<Clock />} />
         <Route path="/timer" element={<Timer />} />
         <Route path="/stopwatch" element={<Stopwatch />} />
       </Routes>
- </BrowserRouter>
+      </div>
+
+
+
   );
 }
 
